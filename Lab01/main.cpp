cd0c7a00ad18579c50fb271e056f824ef2a8e7e2
Eduardo Ribeiro da Silva <erdasilva@indracompany.com>
2016-10-19 20:39:51 -0200

    Lab01

---
+++ Lab01/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+struct student {
+    string name;
+    int age;
+    float marks;
+};
+
+student *initiateStudent(string, int, float);
+
+student *highestScorer(student *stud[], int totalStudents);
+
+int main()
+{
+    int totalStudents = 1;
+    string name;
+    int age;
+    float marks;
+
+    cin >> totalStudents;
+
+    student *stud[totalStudents];
+
+    for(int i = 0; i < totalStudents; i++)
+    {
+        cin >> name >> age >> marks;
+
+        stud[i] = initiateStudent(name,age,marks);
+    }
+
+    student *topper = highestScorer(stud,totalStudents);
+
+    cout << topper->name << " is the topper with " << topper->marks << " marks" << endl;
+
+    return 0;
+}
+
+student *initiateStudent(string name, int age, float marks)
+{
+    student *ret = new student;
+    ret->name = name;
+    ret->age = age;
+    ret->marks = marks;
+
+    return ret;
+}
+
+student *highestScorer(student *stud[], int totalStudents)
+{
+    student *topper = stud[0];
+
+    for (int i = 1; i < totalStudents; i++)
+    {
+        if (stud[i]->marks > topper->marks)
+        {
+            topper = stud[i];
+        }
+    }
+
+    return topper;
+}
